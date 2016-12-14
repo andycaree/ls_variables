16a179855e1b30ece2d4b73576ac20a86ee569a5
Andy March <andycaree@gmail.com>
2016-12-14 22:48:18 +0000

    commit

---
+++ 6.rb
@@ -0,0 +1,9 @@
+# What does the following error message tell you?
+
+
+
+NameError: undefined local variable or method `shoes' for main:Object
+  from (irb):3
+  from /usr/local/rvm/rubies/ruby-2.0.0-rc2/bin/irb:16:in `<main>'
+  
+# there is a variable or method that can't be referenced because it hasn't been defined
